4099ab962a6d0a584445b2b186bf3a6760521d05
Suprith22 <amathisuprith@gmail.com>
2024-11-27 17:16:01 +0530

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	FILE *fp;
+	fp=fopen("INPUT.txt","w+");
+	char c;
+	while(1)
+	{
+		if(c!='\n')
+		{
+			scanf("%c", &c);
+			fputc(c,fp);
+			
+		}
+		else
+		{
+			break;
+		}
+	}
+	fseek(fp,0,0);
+	while((c=getc(fp))!=-1)
+	{
+		printf("%c",c);
+	}
+	fclose(fp);
+	return 0;
+}
